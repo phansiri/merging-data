87c3507b80c807d6a3277750cc2758303ad414a1
phansiri <phansiri@uw.edu>
2014-11-07 11:42:19 -0800

    added functionality plus gitignore for .idea/

--- js/app.js
+++ js/app.js
@@ -5,3 +5,15 @@
     Add code here to create a new Angular application and controller.
     The array of employee objects is already in a global variable named 'pawneeEmployees'
 */
+
+angular.module('AddressBook', [])
+    .controller('AddressController', function($scope) {
+        $scope.employees = pawneeEmployees;
+        $scope.searchString = '';
+        $scope.sortCol = 'lastName';
+        $scope.order = function(colName) {
+            $scope.sortCol = colName;
+        };
+        $scope.phoneVisible = true;
+        $scope.emailVisible = true;
+    });
