0b7e6340ce6b408cd751c22e57b68187e5670c3f
phansiri <phansiri@uw.edu>
2014-11-07 16:12:56 -0800

    cleaned up commented place markers plus deleted blank line for a cleaner look

--- js/app.js
+++ js/app.js
@@ -1,11 +1,3 @@
-/* 
-    app.js
-    Angular application for the address book challenge
-
-    Add code here to create a new Angular application and controller.
-    The array of employee objects is already in a global variable named 'pawneeEmployees'
-*/
-
 angular.module('AddressBook', [])
     .controller('AddressController', function($scope) {
         $scope.employees = pawneeEmployees;
